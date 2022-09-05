00772f5903a6baee5fc39c32a5af648c33f8a69e
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-09-06 02:36:10 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  
+  for( let i = 0; i < array.length ; i++){
+    const difference = target - array[i];
+    for( let j = i + 1 ; j < array.length; j++){
+      if (array[j] === difference) return true;
+    }
+  }
+  return false;
 }
 
 /* 
