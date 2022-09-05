38758fd3525730d9e7b307688384a449a9811d36
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-09-06 02:40:34 +0300

    complete assignment

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function hasTargetSum(array, target) {
   // Write your algorithm here
   
   for( let i = 0; i < array.length ; i++){
+    
     const difference = target - array[i];
     for( let j = i + 1 ; j < array.length; j++){
       if (array[j] === difference) return true;
@@ -12,14 +13,21 @@ function hasTargetSum(array, target) {
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  create a loop to iterate over the item create a variable -> store the difference btn target and array item
+  iterate each item so each number is compared to each other.
+  return true for num maching the difference variable
+  else return false
 */
 
 /*
   Add written explanation of your solution here
+  create nested loop to compare each array item with each of the other items in the array.
+  
 */
 
 // You can run `node index.js` to view these console logs
